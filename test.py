a9d89b1b2deaf12dda3259748f7dfef42201f87c
Oliver Ghysen <oliverghysen@live.com>
2023-03-10 22:52:19 +0100

    first test

---
+++ test.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import os
+import pandas as pd
+import plotly.express as px
+from datetime import datetime as dt
+import matplotlib.pyplot as plt
+from scipy import integrate
+from sklearn.linear_model import LinearRegression
+
+print('hi')
